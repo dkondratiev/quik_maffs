f1c3215be0f68931dac662529e9a3ebf19f43701
Dmitry Kondratiev <dmitry.kondratiev@team.wrike.com>
2020-08-25 22:10:40 +0300

    some sorting

--- arrays.py
+++ arrays.py
@@ -1,4 +1,22 @@
+from typing import Union
+
 def sum_recursive(l: list) -> float:
-    if len(l) == 0:
+    if not l:
         return 0
     return l[0] + sum_recursive(l[1:])
+
+def len_recursive(l: list) -> int:
+    if not l:
+        return 0
+    return 1 + len_recursive(l[1:])
+
+def max_recursive(l: list) -> Union[float, None]:
+    if not l:
+        return None
+    if len(l) == 1:
+        return l[0]
+    rec_max = max_recursive(l[1:])
+    if rec_max > l[0]:
+        return rec_max
+    return l[0]
+
--- sorting.py
+++ sorting.py
@@ -1,9 +1,8 @@
-import time
-import numpy as np
+import random
 
 def is_sorted(l: list) -> bool:
     for i in range(len(l) - 1):
-        if l[i] < l[i + 1]:
+        if l[i] > l[i + 1]:
             return False
     return True
 
@@ -11,11 +10,33 @@ def is_sorted_recursive(l: list) -> bool:
     """
     Quite useless for bigger lists as it will hit recursion limit
     """
-    if len(l) in (0, 1):
+    if len(l) < 2:
         return True
-    if l[0] > l[1]:
+    if l[0] < l[1]:
         return is_sorted_recursive(l[1:])
     else:
         return False
 
-l = sorted(np.random.random(size=100000), reverse=True)
+def quick_sort(l: list) -> list:
+    """
+    Does not reliably work for lists with repeating elements
+    :param l:
+    :return:
+    """
+    if len(l) < 2:
+        return l
+    else:
+        pivot = l[random.randint(0, len(l) - 1)]
+        less = []
+        equal = []
+        greater = []
+        for i in l:
+            if i < pivot:
+                less.append(i)
+            elif i > pivot:
+                greater.append(i)
+            else:
+                equal.append(i)
+        return quick_sort(less) + equal + quick_sort(greater)
+
+print(quick_sort([3, 3, 2, 1, 1]))
