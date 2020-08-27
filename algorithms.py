bd0d00cfb5891e2df2a5fddd8b81e64a76ad19b0
Dmitry Kondratiev <dmitry.kondratiev@team.wrike.com>
2020-08-27 20:39:03 +0300

    euclidean greatest common divisor

---
+++ algorithms.py
@@ -0,0 +1,27 @@
+def euclidean_gcd(a, b):
+    """
+    Finds greatest common divisor of two numbers (Euclidean algorithm)
+    >>> euclidean_gcd(0, 0)
+    0
+    >>> euclidean_gcd(0, 10)
+    10
+    >>> euclidean_gcd(48, 60)
+    12
+    """
+
+    if a == 0 or b == 0:
+        return a + b
+    if a == b:
+        return a
+    if a < b:
+        a, b = b, a
+    mod = a % b
+    if mod == 0:
+        return b
+    return euclidean_gcd(b, mod)
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
